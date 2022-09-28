9e04ad6ec903f1f2caad0a2a56ae7158f30d7543
muathye <muath.ye@gmail.com>
2022-09-28 06:05:01 +0300

    Add D:\GitHub\top-github-users-action\src\helper\markdown\component\license_component.js

---
+++ src/helper/markdown/component/license_component.js
@@ -0,0 +1,14 @@
+let licenseComponent = function() {
+    let create = function(githubUserAndRepository) {
+        let markdown = `## 📄 License\n\n`;
+        markdown = markdown + `- GitHub Action - [${githubUserAndRepository}-action](https://github.com/${githubUserAndRepository}-action)\n`;
+        markdown = markdown + `- Repository - [${githubUserAndRepository}](https://github.com/${githubUserAndRepository})\n`;
+        markdown = markdown + `- Data in the \`./cache\` directory - [Open Database License](https://opendatacommons.org/licenses/odbl/1-0/)\n`;
+        markdown = markdown + `- Code - [MIT](./LICENSE) © [Vite](https://github.com/muath-ye)\n`;
+        return markdown;
+    }
+    return {
+        create: create,
+    };
+}();
+module.exports = licenseComponent;
